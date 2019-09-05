6f0200f63e8ffc462022fed8dba409931ea27e51
nienzu <ibqqz0602@gmail.com>
2019-09-05 16:48:23 +0800

    Add one script setup build environment

---
+++ install_build_require.sh
@@ -0,0 +1,8 @@
+sudo yum update -y
+sudo systemctl disable firewalld.service
+sudo yum install -y sqlite-devel libcurl-devel cppunit-devel libcap-devel libtool libpng-devel gcc-c++
+sudo yum install -y epel-release
+sudo yum install -y nodejs
+sudo yum install -y python-pip
+sudo npm install -g jake
+sudo pip install -g polib
